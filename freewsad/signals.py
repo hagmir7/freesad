2447bf4413357375c03acd5873489ed46a2f535a
Agmir Hassane <hagmir6@gmail.com>
2023-08-12 18:43:29 +0100

    video test

--- freewsad/signals.py
+++ freewsad/signals.py
@@ -132,8 +132,8 @@ def get_quality(width, height):
 @receiver(post_save, sender=Quality)
 def create_quality(sender, instance, created, **kwargs):
     if created:  # Check if a new instance is created
-        # freesad/media
-        file_path = os.path.normpath(f'\\home\\agha6919\\freesad{os.path.join(instance.file.url)}')
+        # freesad/media\\home\\agha6919\\freesad
+        file_path = os.path.normpath(f'{os.path.join(instance.file.url)}')
         video = VideoFileClip(file_path)
         width, height = video.size
         instance.width = width
