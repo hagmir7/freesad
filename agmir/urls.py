a9b92d96e87d1475a65c15b4145ab4bdfa79ebcd
Hassan Agmir <hagmir6@gmail.com>
2023-12-23 17:08:59 +0100

    My commit

--- agmir/urls.py
+++ agmir/urls.py
@@ -42,6 +42,6 @@ urlpatterns += i18n_patterns(
 )
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
