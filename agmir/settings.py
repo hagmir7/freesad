529a0b2d61b32b3962224b2d5a2a10825dc3b65e
Hassan Agmir <hagmir6@gmail.com>
2023-12-23 19:39:15 +0100

    Update

--- agmir/settings.py
+++ agmir/settings.py
@@ -162,7 +162,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT') #'/home/{username}/public_html/media'
 # Google cloud
 
 
