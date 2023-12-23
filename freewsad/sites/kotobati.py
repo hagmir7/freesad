b32d97fd54a012c0babadcd2edfc960da7916987
Hassan Agmir <hagmir6@gmail.com>
2023-12-24 00:10:55 +0100

    New scraping method

--- freewsad/sites/kotobati.py
+++ freewsad/sites/kotobati.py
@@ -21,7 +21,15 @@ def slug(length=8):
 
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Referer": "https://www.google.com/",
+    "DNT": "1",  # Do Not Track header
+    # Add more headers as needed
 }
 
 
