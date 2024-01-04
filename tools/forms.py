ec45d7d8bfa18576d640e8b36a7b6dde4b2f7d14
Hassan Agmir <hagmir6@gmail.com>
2024-01-04 18:32:45 +0100

    Add facebook group validation url

--- tools/forms.py
+++ tools/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import *
+from django.utils.translation import gettext_lazy as _
 
 
 class LinkForm(forms.ModelForm):
@@ -11,19 +12,29 @@ class LinkForm(forms.ModelForm):
             "link": forms.TextInput(attrs={"class": "form-control"}),
             "custom": forms.TextInput(attrs={"class": "form-control"}),
             "image": forms.FileInput(attrs={"class": "form-control"}),
-            "start": forms.NumberInput(attrs={"class": "form-control", "min": 1, "max": 60}),
-            "end": forms.NumberInput(attrs={"class": "form-control", "min": 1, "max": 60}),
+            "start": forms.NumberInput(
+                attrs={"class": "form-control", "min": 1, "max": 60}
+            ),
+            "end": forms.NumberInput(
+                attrs={"class": "form-control", "min": 1, "max": 60}
+            ),
             "description": forms.Textarea(attrs={"class": "form-control", "rows": 5}),
         }
-
 
 
 class FacebookGroupForm(forms.ModelForm):
     class Meta:
         model = FacebookGroup
-        fields = '__all__'
+        fields = "__all__"
+
+    def clean_url(self):
+        url = self.cleaned_data.get("url")
+        if url and FacebookGroup.objects.filter(url=url).exists():
+            raise forms.ValidationError(_("The group already exists."))
+        return url
+
 
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = '__all__'
+        fields = "__all__"
